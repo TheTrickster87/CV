aee5dfce6b56fb2b0fe3cbd71ad0b935ed248648
Nik-PC\Nik <gandolfi.niccol@gmail.com>
2020-03-15 18:29:10 +0100

    Upload Cms
    up cms

---
+++ cms-backoffice/precache-manifest.6dda634e67aa5e1b35364a4073f69add.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec14238e00efd2a204c750fe0da26efa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8e875b3206001743dda",
+    "url": "./static/css/2.806183df.chunk.css"
+  },
+  {
+    "revision": "272ba9ff14c5c07c0fbb",
+    "url": "./static/css/main.0f8d68ca.chunk.css"
+  },
+  {
+    "revision": "f8e875b3206001743dda",
+    "url": "./static/js/2.aed2891a.chunk.js"
+  },
+  {
+    "revision": "a020f216e4afab3c22ceba53fe916399",
+    "url": "./static/js/2.aed2891a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "272ba9ff14c5c07c0fbb",
+    "url": "./static/js/main.2e14e869.chunk.js"
+  },
+  {
+    "revision": "dcb9c820b00521c2cee8",
+    "url": "./static/js/runtime-main.9682a541.js"
+  }
+]);
