7aadf112b22538abc1843921a0d072242b8f4bb5
Nik-PC\Nik <gandolfi.niccol@gmail.com>
2020-03-15 18:49:54 +0100

    tris
    tris

---
+++ tris/precache-manifest.66c735fe7cbbde7a6292b25feb155a30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1af4fc7268b4e1565ed1bd957168bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76f639ce7a63e808bb76",
+    "url": "./static/css/main.79d72a7e.chunk.css"
+  },
+  {
+    "revision": "fa00e5a7abbddf3372e1",
+    "url": "./static/js/2.39cb3d1a.chunk.js"
+  },
+  {
+    "revision": "76f639ce7a63e808bb76",
+    "url": "./static/js/main.ad8baadd.chunk.js"
+  },
+  {
+    "revision": "e7e7d897dbd6c57f9040",
+    "url": "./static/js/runtime-main.44d4c2bb.js"
+  }
+]);
