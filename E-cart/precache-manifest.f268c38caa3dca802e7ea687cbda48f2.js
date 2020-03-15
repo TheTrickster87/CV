15af7618c6c5c88d35134db77839382b0ba6fb10
Nik-PC\Nik <gandolfi.niccol@gmail.com>
2020-03-15 18:07:02 +0100

    new upload
    new upload

---
+++ E-cart/precache-manifest.f268c38caa3dca802e7ea687cbda48f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a16104199579bea12c1edd5ed912b35",
+    "url": "./index.html"
+  },
+  {
+    "revision": "729f73802bdf4144c75d",
+    "url": "./static/css/main.159cdb8e.chunk.css"
+  },
+  {
+    "revision": "8c728f85519edab96db2",
+    "url": "./static/js/2.7334daa4.chunk.js"
+  },
+  {
+    "revision": "729f73802bdf4144c75d",
+    "url": "./static/js/main.12423c70.chunk.js"
+  },
+  {
+    "revision": "bc37991ea93b3e90ccc0",
+    "url": "./static/js/runtime-main.ad6dfcd4.js"
+  }
+]);
