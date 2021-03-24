bd1201d0dff4e29871483dcfb55c6d3e3bb76222
TheTrickster87 <46546387+TheTrickster87@users.noreply.github.com>
2021-03-24 18:49:27 +0100

    Uploading Todo App
    Uploading Todo App- build 24/03/21

---
+++ todo-app/precache-manifest.ca9b87faa1e94575a119e06799086d25.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bccfd97463fb06a2178fa0b9457d61b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a0c6e572926e105666f",
+    "url": "./static/css/main.ceac5a97.chunk.css"
+  },
+  {
+    "revision": "5c1394b1c975d36118b9",
+    "url": "./static/js/2.ee65917f.chunk.js"
+  },
+  {
+    "revision": "8a0c6e572926e105666f",
+    "url": "./static/js/main.d365a483.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5742160ca6ea943a508cc5763e8cd312",
+    "url": "./static/media/icon-check.5742160c.svg"
+  },
+  {
+    "revision": "e66b8c621b1f209e44a7c955c0e5e809",
+    "url": "./static/media/icon-moon.e66b8c62.svg"
+  },
+  {
+    "revision": "b3182f3b0ccf1d65cae48b64861b83a9",
+    "url": "./static/media/icon-sun.b3182f3b.svg"
+  },
+  {
+    "revision": "8595f769e414b4a97ee691200078315b",
+    "url": "./static/media/unselected.8595f769.svg"
+  }
+]);
